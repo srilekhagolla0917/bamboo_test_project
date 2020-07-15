2f49a7dbacecbffafaa3dafd41127fc64e586c26
Srilekha Golla <SGolla@air-worldwide.com>
2020-07-15 20:28:19 +0530

    changes to fail

--- src/app/tab1/tab1.page.spec.ts
+++ src/app/tab1/tab1.page.spec.ts
@@ -22,4 +22,8 @@ describe('Tab1Page', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should fail', () => {
+    expect(component).toBeFalsy();
+  })
 });
